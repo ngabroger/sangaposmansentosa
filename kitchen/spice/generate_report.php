d99d45ac634226ae11a2839365e0a730e00b0caf
ngabroger <arbination13@gmail.com>
2024-11-30 15:05:53 +0700

    update

--- kitchen/spice/generate_report.php
+++ kitchen/spice/generate_report.php
@@ -52,8 +52,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
 
     <!-- Include jQuery and DataTables JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
+   
+    
     <script>
         $(document).ready(function() {
             $('#reportTable').DataTable();
