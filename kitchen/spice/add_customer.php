bdcbdfb643dc229bf4c9bc417f780bfcafbfd8ef
ngabroger <arbination13@gmail.com>
2025-05-20 12:37:52 +0700

    feat

--- kitchen/spice/add_customer.php
+++ kitchen/spice/add_customer.php
@@ -12,11 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $description = $_POST['description'];
     $nama_sales = $_POST['nama_sales'];
     $area_lokasi = $_POST['area_lokasi'];
-    $current_date = date('Y-m-d'); // Current date as default value for recent_date_order
+    $query = $conn->prepare("INSERT INTO customer (id_toko, nama_toko, alamat, no_hp, owner, system_pembayaran, link_toko,description,nama_sales,area_lokasi) VALUES (?, ?, ?, ?, ?,?,?,?,?,?)");
 
-    $query = $conn->prepare("INSERT INTO customer (id_toko, nama_toko, alamat, no_hp, owner, system_pembayaran, link_toko, description, nama_sales, area_lokasi, recent_date_order) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-
-    $query->bind_param("sssssssssss", $id_toko, $nama_toko, $alamat, $no_hp, $owner, $system_pembayaran, $link_alamat, $description, $nama_sales, $area_lokasi, $current_date);
+    $query->bind_param("ssssssssss", $id_toko, $nama_toko, $alamat, $no_hp, $owner, $system_pembayaran, $link_alamat, $description, $nama_sales, $area_lokasi);
 
     if ($query->execute()) {
         echo "<script>alert('Berhasil Ditambahkan.');window.location='../customer_subscribe.php';</script>";
